d707b3f6a89ecb07727b45d9f4134fd89f755dcc
Mikhaylov_DV <dmikhailov@mail.ru>
2018-06-25 15:42:12 +0300

    HW 2 добавлена проверка на переполнение в save, предупреждения для delete, update, get (если не найдены) переделан clear (запись null)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -12,45 +12,51 @@ public class ArrayStorage {
     }
 
     void clear() {
-        storage = new Resume[10000];
+        Arrays.fill(storage, null);
         size = 0;
     }
 
     void save(Resume r) {
-        if (!update(r)) {
-            storage[size] = r;
-            size++;
-        }
-
+        if (r == null)
+            return;
+        if (!update(r))
+            if (storage.length == size)
+                System.out.println("Resume did not saved! Overflow.");
+            else {
+                storage[size] = r;
+                size++;
+                return;
+            }
    }
 
    boolean update(Resume r) {
-        if (r == null)
-            return false;
-       for (int i = 0; i < size; i++) {
+       if (r == null)
+           return false;
+       for (int i = 0; i < size; i++)
            if (storage[i].uuid.equals(r.uuid)) {
                storage[i] = r;
                return true;
            }
-       }
        return false;
    }
-
 
     Resume get(String uuid) {
         for (int j = 0; j < size; j++)
             if (storage[j].uuid.equals(uuid))
                 return storage[j];
+        System.out.println("Resume not found.");
         return null;
     }
 
     void delete(String uuid) {
         for (int i = 0; i < size; i++)
             if (storage[i].uuid.equals(uuid)) {
-                System.arraycopy(storage, i+1, storage, i, size - i);
+                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
                 size--;
-                break;
+                return;
             }
+        System.out.println("Resume not found.");
+        return;
     }
 
 
