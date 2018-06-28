6cd2c363ccb11c2f6cf25c9e8b07573bae610642
Mikhaylov_DV <dmikhailov@mail.ru>
2018-06-28 18:19:14 +0300

    HW 3 fix getIndex

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -30,15 +30,12 @@ public class ArrayStorage extends AbstractArrayStorage{
 
 
     protected int getIndex(String uuid) {
-        Resume searchKey = new Resume();
-        searchKey.setUuid(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
-/*        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
         return -1;
-*/
+
     }
 }
